6be1a32dcb51c1c783b646fc5958c98ac0496038
Hyuksanan <ahn934@naver.com>
2023-02-04 21:36:47 +0900

    currency change , 3'd commit

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@ print('Hellow An')
 network_status_flag=True
 timer_flag = False
 interrupt_check = False
-default_currency_dic = {'KRW': 1, 'USD': 1140, 'PLN': 305, 'CNY': 160, 'JPY': 9, 'EUR': 1221}
+default_currency_dic = {'KRW': 1, 'USD': 1200, 'PLN': 305, 'CNY': 160, 'JPY': 9, 'EUR': 1221}
 
 # default_currency_key_list -->  default_currency_dic.keys()는 순서가 없어 list로 순서를 정의 (2022.12.27)
 default_currency_key_list = ['KRW', 'USD', 'PLN', 'CNY', 'JPY', 'EUR']
